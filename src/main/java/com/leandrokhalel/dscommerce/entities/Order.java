95e58be1b708a20c5dc542314c40052043d60658
leopimentl <leandrokhalelpimentel@gmail.com>
2023-11-13 15:33:47 -0300

    Orde and, OrderStatus

---
+++ src/main/java/com/leandrokhalel/dscommerce/entities/Order.java
@@ -0,0 +1,32 @@
+package com.leandrokhalel.dscommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    @Enumerated
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User client;
+}
---
+++ src/main/java/com/leandrokhalel/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.leandrokhalel.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}
