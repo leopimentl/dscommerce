9f3f383c054ddf9c08f165e7f2b904b8bded3ef1
leopimentl <leandrokhalelpimentel@gmail.com>
2023-11-14 00:53:56 -0300

    product and category relationship

--- src/main/java/com/leandrokhalel/dscommerce/entities/Category.java
+++ src/main/java/com/leandrokhalel/dscommerce/entities/Category.java
@@ -6,17 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_category")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class User {
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,16 +23,6 @@ public class User {
 
     private String name;
 
-    @Column(unique = true)
-    private String email;
-
-    private String phone;
-
-    private LocalDate birthDate;
-
-    private String password;
-
-    @OneToMany(mappedBy = "client")
-    private List<Order> orders = new ArrayList<>();
-
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 }
---
+++ src/main/java/com/leandrokhalel/dscommerce/entities/Product.java
@@ -0,0 +1,41 @@
+package com.leandrokhalel.dscommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private Double price;
+
+    private String imgUrl;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+
+}
