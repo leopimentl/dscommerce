4843537721ce58ce7a6eccb2a03f42eea3ffe535
leopimentl <leandrokhalelpimentel@gmail.com>
2023-11-15 00:15:22 -0300

    OrderItem and OrderItemPK

---
+++ src/main/java/com/leandrokhalel/dscommerce/entities/OrderItemPK.java
@@ -0,0 +1,26 @@
+package com.leandrokhalel.dscommerce.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+public class OrderItemPK {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
+
+
+
+
