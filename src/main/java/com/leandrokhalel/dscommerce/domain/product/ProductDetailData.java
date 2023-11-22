3c06ac07a6ab158b0bee0db04abb8247773abae5
leopimentl <leandrokhalelpimentel@gmail.com>
2023-11-21 23:07:55 -0300

    Correction

--- src/main/java/com/leandrokhalel/dscommerce/domain/product/ProductDetailData.java
+++ src/main/java/com/leandrokhalel/dscommerce/domain/product/ProductDetailData.java
@@ -1,12 +1,13 @@
 package com.leandrokhalel.dscommerce.domain.product;
 
 public record ProductDetailData(
+        Long id,
         String name,
         String description,
         Double price,
         String imgUrl) {
 
     public ProductDetailData(Product p) {
-        this(p.getName(), p.getDescription(), p.getPrice(), p.getImgUrl());
+        this(p.getId(), p.getName(), p.getDescription(), p.getPrice(), p.getImgUrl());
     }
 }
--- src/main/java/com/leandrokhalel/dscommerce/domain/product/ProductRegistrationData.java
+++ src/main/java/com/leandrokhalel/dscommerce/domain/product/ProductRegistrationData.java
@@ -3,7 +3,6 @@ package com.leandrokhalel.dscommerce.domain.product;
 
 
 public record ProductRegistrationData(
-        Long id,
         String name,
         String description,
         Double price,
